6d5afabb14653a2a29d9d12d8b4649183e84251f
hi pancake <flowerishome@gmail.com>
2024-07-07 23:14:10 -0400

    push

--- module-7/movies_queries.py
+++ module-7/movies_queries.py
@@ -23,21 +23,21 @@ cursor.execute("SELECT * FROM genre")
 print("DISPLAYING GENRES")
 genres = cursor.fetchall()
 for genre in genres:
-    print(f"Studio ID: {genre[0]}")
+    print(f"Genre ID: {genre[0]}")
     print(f"Genre Name is: {genre[1]}")
 
 cursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < 120")
 print("DISPLAYING MOVIES UNDER 2 HOURS")
 twohour = cursor.fetchall()
 for movie in twohour:
-    print(f"Studio ID: {movie[1]}")
+    print(f"Movie Runtime: {movie[1]}")
     print(f"Movie Name is: {movie[0]}")
 
 cursor.execute("SELECT film_name, film_director FROM film GROUP BY film_name, film_director ORDER BY film_director")
 director_data = cursor.fetchall()
 print("DISPLAYING DIRECTORS")
 for film in director_data:
-    print(f"Studio ID: {film[0]}")
+    print(f"Film Name: {film[0]}")
     print(f"Director Name is: {film[1]}")
 
 
